5312b6d7aec737e93f20d5c37bcb610206132831
szaratep <sebaszarate2005@gmail.com>
2026-07-03 20:28:09 -0500

    feat(/features/contact): Se crea el componende de contact completo y linkeado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const routes: Routes = [
     {path: 'home', loadComponent: () => import('./features/home/home')},
     {path: 'products', loadComponent: () => import('./features/products/products')},
     {path: 'customized', loadComponent: () => import('./features/customized/customized')},
+    {path: 'contact', loadComponent: () => import('./features/contact/contact'),},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
